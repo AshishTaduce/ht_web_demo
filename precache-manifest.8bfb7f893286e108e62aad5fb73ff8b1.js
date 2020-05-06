8c43cb75d8476be93d5443354daf03011e71fc22
AshishTaduce <ashishteddy2@gmail.com>
2020-05-06 17:21:14 +0530

    initial commit

---
+++ precache-manifest.8bfb7f893286e108e62aad5fb73ff8b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d9bf9b90c001ea23f7b51b69d08daf5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37c690c8fc71e4f19da9",
+    "url": "/static/css/main.6cdc9e2d.chunk.css"
+  },
+  {
+    "revision": "1eb3590d4f23126aa4e7",
+    "url": "/static/js/2.c4fe823c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c4fe823c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37c690c8fc71e4f19da9",
+    "url": "/static/js/main.e81cc956.chunk.js"
+  },
+  {
+    "revision": "13277f473baf9d6bd557",
+    "url": "/static/js/runtime-main.24017274.js"
+  },
+  {
+    "revision": "6843fce189ed9ec0f6d3bf6c8debaf4f",
+    "url": "/static/media/comment-24px.6843fce1.svg"
+  }
+]);
